f49b769c3883d7e8d12ae2671612da247239e091
Robb Barrows <robb.barrows@gmail.com>
2021-02-17 22:59:58 +0000

    Running setup.sh will create a bridge that can be used with ssl.

---
+++ tutorials/kube-mqtt/gen_keys.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -x
+# This was mostly havested from:
+# http://www.steves-internet-guide.com/mosquitto-tls/
+
+SERVER_IP=35.226.184.19
+CERTS_DIR=base/certs/
+
+# Clear old ones
+rm -rf $CERTS_DIR/*
+mkdir -p $CERTS_DIR
+
+# Create openssl config file
+cat << EOF > openssl.conf
+[req]
+default_bits=2048
+encrypt_key=no
+default_md=sha256
+distinguished_name=req_subj
+prompt=no
+[req_subj]
+commonName="$SERVER_IP"
+emailAddress=""
+countryName=US
+stateOrProvinceName=Wisconsin
+localityName=Milwaukee
+organizationName="BnS"
+organizationalUnitName="Concept Team"
+EOF
+
+# Create key pair for CA
+openssl genrsa -des3 -out $CERTS_DIR/ca.key 2048
+
+# Certificate for CA using CA key
+sed -i "s/organizationalUnitName=.*/organizationalUnitName=\"Concept Team CA Cert\"/" openssl.conf
+openssl req -new -config openssl.conf -x509 -days 1826 -key $CERTS_DIR/ca.key -out $CERTS_DIR/ca.crt
+
+# server key pair that will be used by the MQTT broker
+openssl genrsa -out $CERTS_DIR/server.key 2048
+
+# Create a certificate request csr
+# Common name is the IP or FQDN of the MQTT broker
+sed -i "s/organizationalUnitName=.*/organizationalUnitName=\"Concept Team Server Key\"/" openssl.conf
+openssl req -new -config openssl.conf -out $CERTS_DIR/server.csr -key $CERTS_DIR/server.key
+
+# use CA to verify and sign the server certificate, this creates the server.crt
+openssl x509 -req -in $CERTS_DIR/server.csr -CA $CERTS_DIR/ca.crt -CAkey $CERTS_DIR/ca.key -CAcreateserial -out $CERTS_DIR/server.crt -days 360
+
+rm openssl.conf
+# You should now have the following 3 needed files:
+# ca.cert server.crt server.key 
+
---
+++ tutorials/kube-mqtt/setup.sh
@@ -0,0 +1,73 @@
+#!/bin/bash -x
+
+# This is all based off of the tutorial here:
+# https://cloud.google.com/community/tutorials/kube-mqtt
+# You are expected to have the following done already:
+# 1. Kube cluster already created.
+# 2. Applicable API's enabled
+# 3. You are in the chosed gcloud project
+
+export PROJECT=$(gcloud config list project --format "value(core.project)")
+export REGION=us-central1
+export REGISTRY=gadgets
+export DEVICE=bridge
+
+# Gen keys for SSL
+gen_keys.sh
+
+# Delete existing deployment, we are starting from scratch
+kubectl delete --wait -n default deployment iot-core-bridge
+
+# Create IoT Core resources if needed
+gcloud pubsub topics delete device-events
+gcloud pubsub subscriptions delete debug
+gcloud iot devices delete $DEVICE --region $REGION --registry $REGISTRY --quiet 
+gcloud iot registries delete $REGISTRY --region=$REGION --quiet
+
+# Build the bridge manager container# 
+pushd refresher-container
+gcloud builds submit --tag gcr.io/$PROJECT/refresher .
+popd
+
+# Create the device keys
+pushd bridge
+rm -rf *.pem
+openssl ecparam -genkey -name prime256v1 -noout -out ec_private.pem
+openssl ec -in ec_private.pem -pubout -out ec_public.pem
+popd
+
+sleep 1
+
+gcloud pubsub topics create device-events
+sleep 1
+gcloud pubsub subscriptions create debug --topic device-events
+sleep 1
+
+gcloud iot registries create $REGISTRY \
+--region=$REGION \
+--event-notification-config topic=device-events
+
+sleep 1
+gcloud iot devices create $DEVICE \
+--region $REGION \
+--registry $REGISTRY \
+--public-key path=bridge/ec_public.pem,type=es256-pem
+
+# Get the specific address for the bridge manager container# 
+IMAGE=$(gcloud container images describe gcr.io/$PROJECT/refresher --format="value(image_summary.fully_qualified_digest)")
+
+# setup all the variables fro the bridge
+sed -i "s/PROJECT_ID:.*/PROJECT_ID: $PROJECT/"            bridge/device-config.yaml
+sed -i "s/REGISTRY_ID:.*/REGISTRY_ID: $REGISTRY/"         bridge/device-config.yaml
+sed -i "s/BRIDGE_DEVICE_ID:.*/BRIDGE_DEVICE_ID: $DEVICE/" bridge/device-config.yaml
+sed -i "s/CLOUD_REGION:.*/CLOUD_REGION: $REGION/"         bridge/device-config.yaml
+sed -i "s|image:.*|image: $IMAGE|"                        bridge/project-image.yaml
+
+# Deploy the bridge
+kubectl apply -k bridge/
+
+# Wait for bridge
+sleep 1
+kubectl wait --for=condition=Ready pod -l "app=iot-core-bridge"
+
+
